afc2fd582f28bcf854d4eb13c962592c0895e910
yihang.wang@nlpr.ia.ac.cn <yihang.wang@nlpr.ia.ac.cn>
2024-10-09 12:01:38 +0800

    update

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setuptools.setup(
     name="OpenDTB-main",
     version="0.2.6",
     description="CASIA's digital twin brain platform",
-    author="Brain Net Group",
+    author="Brain Netom Group",
     url="https://github.com/OpenDTB/OpenDTB-MAIN",
     license="GPL-3.0",
     install_requires=[
-        "opendtb-framework @ https://github.com/OpenDTB/OpenDTB-Framework/tarball/main",
+        "OpenDTB-Framework @ https://github.com/OpenDTB/OpenDTB-Framework/tarball/main",
         "numba",
         "mako",
         "brainpy",
